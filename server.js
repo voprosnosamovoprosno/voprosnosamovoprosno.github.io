b1e65e93db7c5730a7c1a4c3ab4b9d5687ded359
Glitch (voprosnosamovoprosno) <none>
2024-02-10 10:41:50 +0000

    ?вопросносамовопросно?

--- server.js
+++ server.js
@@ -1,68 +1,51 @@
-/**
- * This is the main server script that provides the API endpoints
- *
- * Uses sqlite.js to connect to db
- */
 const path = require("path");
-
 const fastify = require("fastify")({
   // Set this to true for detailed logging:
   logger: false
 });
-
 // Setup our static files
 fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
   prefix: "/", // optional: default '/'
 });
-  
 fastify.register(require("@fastify/formbody"));
-
 // point-of-view is a templating manager for fastify
 fastify.register(require("@fastify/view"), {
   engine: {
     handlebars: require("handlebars"),
   },
 });
-
 const db = require("./sqlite.js");
 const errorMessage =
   "Whoops! Error connecting to the database–please try again!";
-
 // OnRoute hook to list endpoints
 const routes = { endpoints: [] };
 fastify.addHook("onRoute", routeOptions => {
   routes.endpoints.push(routeOptions.method + " " + routeOptions.path);
 });
 
-fastify.get("/", (request, reply) => {
-  return reply.view('voprosnosamovoprosno.html');
-});
-
 var vssgml=null;
-
 async function vssmsf()
    {let vssbvl=false;
-    vssgml = await db.getMessages();
+    vssgml = await db.vssgetf();
     console.log('?vssgml.length'+vssgml.length+'?');
     for(let vssj=0;vssj<vssgml.length;vssj++)
       {if(vssj<vssgml.length-12)
-        {vssdml=await db.deleteMessage(vssgml[vssj].id);
+        {vssdml=await db.vssdelf(vssgml[vssj].vssid);
          vssbvl=true;
-         console.log('?vssdelmesid'+vssgml[vssj].id+vssdml+'?');
-        }else{if(vssgml[vssj].message[0]!='?')
-               {vssdml=await db.deleteMessage(vssgml[vssj].id);
+         console.log('?vssqntdelid'+vssgml[vssj].vssid+vssdml+'?');
+        }else{if(vssgml[vssj].vsstxt[0]!='?')
+               {vssdml=await db.vssdelf(vssgml[vssj].vssid);
                 vssbvl=true;
-                console.log('?vssdelarrmesid'+vssgml[vssj].id+vssdml+'?');
+                console.log('?vsscntdelid'+vssgml[vssj].vssid+vssdml+'?');
                }
              }
       }   
     if(vssbvl==true)
-     {vssgml = await db.getMessages();
-      console.log('?vssmsgsqnt'+vssgml.length+'?');
+     {vssgml = await db.vssgetf();
+      console.log('?vssgml.length'+vssgml.length+'?');
      }
   }
-
 fastify.post("/", async (request, reply) => {
   console.log('?'+request.body+'?');
   if(vssgml==null){await vssmsf();}
@@ -72,33 +55,31 @@ fastify.post("/", async (request, reply) => {
      console.log('?vssindxvl'+vssindxvl+'?');
     }
   if(vssindxvl>=0)
-  {return vssgml[vssindxvl].message;}  
+  {return vssgml[vssindxvl].vsstxt;}  
 });
-
 fastify.post("/vssadd", async (request, reply) => {
-  let vssaml = await db.addMessage(request.body);
-  console.log('?vssaddmessage'+vssaml+'?');
+  let vssaml = await db.vssaddf(request.body);
+  console.log('?vssadd'+vssaml+'?');
   console.log(request.body);
   vssmsf();
 });
-
 fastify.get("/vssxs", async (request, reply) => {
   console.log('?vssxs?');
 });
-
+fastify.get("/", (request, reply) => {
+  return reply.view('voprosnosamovoprosno.html');
+});
+fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`?vss${address}?`);
+});
+/*
 // Update text for an message (auth)
 fastify.put("/message", async (request, reply) => { 
-  console.log(request.body);
-  return replay.view('voprosnosamovoprosno9.html');
-  /*let data = {};
-  const auth = authorized(request.headers.admin_key);
-  if(!auth || !request.body || !request.body.id || !request.body.message) data.success = false;
-  else data.success = await db.updateMessage(request.body.id, request.body.message); 
-  const status = data.success ? 201 : auth ? 400 : 401;
-  reply.status(status).send(data);
-  */
 });
-
 // Delete a message (auth)
 fastify.delete("/message", async (request, reply) => {
   let data = {};
@@ -108,7 +89,6 @@ fastify.delete("/message", async (request, reply) => {
   const status = data.success ? 201 : auth ? 400 : 401;
   reply.status(status).send(data);
 });
-
 const vsskvn='vsskvv';
 // Helper function to authenticate the user key
 const authorized = key => {
@@ -121,12 +101,5 @@ const authorized = key => {
     return false;
   else return true;
 };
-
+*/
 // Run the server and report out to the logs
-fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`?vss${address}?`);
-});
--- sqlite.js
+++ sqlite.js
@@ -1,18 +1,10 @@
-/**
- * Module handles database management
- *
- * The sample data is for a chat log with one table:
- * Messages: id + message text
- */
-
 const fs = require("fs");
-const dbFile = "./.data/chat.db";
+const dbFile = "./.data/vss.db";
 const exists = fs.existsSync(dbFile);
 const sqlite3 = require("sqlite3").verbose();
 const dbWrapper = require("sqlite");
 const casual = require("casual");
 let db;
-
 //SQLite wrapper for async / await connections https://www.npmjs.com/package/sqlite
 dbWrapper
   .open({
@@ -25,40 +17,48 @@ dbWrapper
     try {
       if (!exists) {
         await db.run(
-          "CREATE TABLE Messages (id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)"
+          "CREATE TABLE vsst (vssid INTEGER PRIMARY KEY AUTOINCREMENT, vsstxt TEXT)"
         );
       }
     } catch (dbError) {
       console.error(dbError);
     }
   });
-
 // Server script calls these methods to connect to the db
 module.exports = {
   
-  // Get the messages in the database
-  getMessages: async () => {
+  vssgetf: async () => {
     try {
-      return await db.all("SELECT * from Messages");
+      return await db.all("SELECT * from vsst");
     } catch (dbError) {
       console.error(dbError);
     }
   },
-
-  // Add new message
-  addMessage: async message => {
+  
+  vssaddf: async vsstxtfp => {
     let success = false;
     try {
-      success = await db.run("INSERT INTO Messages (message) VALUES (?)", [
-        message
+      success = await db.run("INSERT INTO vsst (vsstxt) VALUES (?)", [
+        vsstxtfp
       ]);
     } catch (dbError) {
       console.error(dbError);
     }
     return success.changes > 0 ? true : false;
   },
-
-  // Update message text
+  
+  vssdelf: async vssidfp => {
+    let success = false;
+    try {
+      success = await db.run("Delete from vsst WHERE vssid = ?", vssidfp);
+    } catch (dbError) {
+      console.error(dbError);
+    }
+    return success.changes > 0 ? true : false;
+  }
+};
+  
+/*// Update message text
   updateMessage: async (id, message) => {
     let success = false;
     try {
@@ -71,16 +71,5 @@ module.exports = {
       console.error(dbError);
     }
     return success.changes > 0 ? true : false;
-  },
-
-  // Remove message
-  deleteMessage: async id => {
-    let success = false;
-    try {
-      success = await db.run("Delete from Messages WHERE id = ?", id);
-    } catch (dbError) {
-      console.error(dbError);
-    }
-    return success.changes > 0 ? true : false;
   }
-};
+*/
